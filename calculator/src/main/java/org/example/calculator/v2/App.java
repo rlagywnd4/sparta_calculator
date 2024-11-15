874d784d00edeeab68478673168ceec0d736ab6e
rlagywnd4 <rlagywnd4@gmail.com>
2024-11-15 20:00:13 +0900

    feat : add existing Level 1 functionality
    App.java:
        기존 레벨 1 로직 추가
    Calculator.java:
        레벨 1과 동일한 결과를 위한 출력 기능 추가
        결과 저장을 위한 큐 추가

---
+++ calculator/src/main/java/org/example/calculator/v2/App.java
@@ -0,0 +1,38 @@
+package org.example.calculator.v2;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator(new LinkedList<>());
+
+        boolean isRunning = true;
+        while (isRunning) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int firstNumber = scanner.nextInt();
+            if (firstNumber < 0) {
+                System.out.println("양의 정수를 입력해주세요");
+                continue;
+            }
+
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int secondNumber = scanner.nextInt();
+            if (secondNumber < 0) {
+                System.out.println("양의 정수를 입력해주세요");
+                continue;
+            }
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = scanner.next().charAt(0);
+
+            calculator.results.add(calculator.calculate(operator, firstNumber, secondNumber));
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            isRunning = !scanner.next().equals("exit");
+
+        }
+
+    }
+}
--- calculator/src/main/java/org/example/calculator/v2/Calculator.java
+++ calculator/src/main/java/org/example/calculator/v2/Calculator.java
@@ -1,19 +1,30 @@
 package org.example.calculator.v2;
 
+import java.util.Queue;
+
 public class Calculator {
+    Queue<Integer> results;
+
+    Calculator(Queue<Integer> results){
+        this.results = results;
+    }
 
     // 연산 결과를 저장하는 컬렉션 타입 필드
     // 사칙연산 수행후, 결과값 반환 메서드 구현
     public int calculate(char operator, int firstNumber, int secondNumber){
         switch (operator) {
             case '+':
+                System.out.println("결과 : " + (firstNumber + secondNumber));
                 return (firstNumber + secondNumber);
             case '-':
+                System.out.println("결과 : " + (firstNumber - secondNumber));
                 return  (firstNumber - secondNumber);
             case '*':
+                System.out.println("결과 : " + (firstNumber * secondNumber));
                 return  (firstNumber * secondNumber);
             case '/':
                 if (secondNumber != 0) {
+                    System.out.println("결과 : " + (firstNumber / secondNumber));
                     return (firstNumber / secondNumber);
                 }else {
                     System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
