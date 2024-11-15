88ac070e3dc1448a31ed992d1c0dcab2e61500a9
rlagywnd4 <rlagywnd4@gmail.com>
2024-11-15 15:34:49 +0900

    chore: Remove unnecessary comments from Calculator.java

--- calculator/src/main/java/org/example/calculator/Calculator.java
+++ calculator/src/main/java/org/example/calculator/Calculator.java
@@ -3,21 +3,6 @@ package org.example.calculator;
 import java.util.Scanner;
 
 public class Calculator {
-    //        입력
-//          양의 정수 입력 받기
-//              양의 정수 2개(0 포함)
-//          사칙연산 기호 입력받기
-//              char 타입의 사칙연산 기호
-//        연산 진행
-//          사칙연산 기호에 따른 연산 진행
-//              switch문으로 연산기호 구분
-//          연산 오류 방지
-//              예상 오류에 대한 내용 출력
-//        결과 출력
-//          연산 결과 출력
-//        계산 반복
-//          반복 여부 입력 받기
-//          "exit"입력시 반복 종료
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
